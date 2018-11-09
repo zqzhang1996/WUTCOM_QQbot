322785225b387cd356bcaa18aee8c9340b112526
zqzhang1996 <Pr0phet.chn@outlook.com>
2018-11-10 01:11:56 +0800

    定时10秒解除禁言

--- yingKill.cpp
+++ yingKill.cpp
@@ -1,16 +1,24 @@
 #include <CQEVE_GroupMsg.h>
 #include <CQAPI_EX.h>
+#include <thread>
 
 #include <string>
 
 using namespace std;
 using namespace CQ;
 
+void unBan(int group, int qq,int time) {
+	Sleep(time);
+	setGroupBan(group, qq, 0);
+}
+
 EVE_GroupMsg_EX(YingKill) {
 	auto message = eve.message;
 	auto msg = eve.sendMsg();
 	if (message.find("嘤") != string::npos) {
 		msg << "一拳一个嘤嘤怪" << send;
-		setGroupBan(eve.fromGroup, eve.fromQQ, 10);
+		setGroupBan(eve.fromGroup, eve.fromQQ, 10);//按秒接收参数，转换成分向上取整
+		thread t(unBan, eve.fromGroup, eve.fromQQ, 10000);//毫秒
+		t.detach();
 	}
 }
