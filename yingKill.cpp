7719e5a5fb8b549dbf05a05df2b2a0afe5a49c87
zqzhang1996 <Pr0phet.chn@outlook.com>
2018-11-13 15:45:50 +0800

    修正系统消息判断，取消定时解除禁言

--- yingKill.cpp
+++ yingKill.cpp
@@ -14,7 +14,7 @@ void unBan(long long group, long long qq, int time) {
 }
 
 EVE_GroupMsg_EX(YingKill) {
-	if (eve.fromQQ == getLoginQQ() || eve.fromQQ == 1000000) {
+	if (eve.fromQQ == getLoginQQ() || eve.isSystem()) {
 		return;
 	}
 	auto message = eve.message;
@@ -22,7 +22,9 @@ EVE_GroupMsg_EX(YingKill) {
 	if (message.find("嘤") != string::npos) {
 		msg << "一拳一个嘤嘤怪" << code::at(eve.fromQQ) << send;
 		setGroupBan(eve.fromGroup, eve.fromQQ, 10);//按秒接收参数，转换成分向上取整
-		thread t(unBan, eve.fromGroup, eve.fromQQ, 10 * 1000);//毫秒
-		t.detach();
+
+		//thread t(unBan, eve.fromGroup, eve.fromQQ, 10 * 1000);//毫秒
+		//t.detach();
+
 	}
 }
